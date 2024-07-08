2211ee05beab74e21d4cc0121af1f5b0a894c2b0
LeoBang <bluke8489@gmail.com>
2024-07-08 22:21:32 +0900

    infra: terraform NCP provider 설정 및 플러그인 설치
    - .terraform.lock.hcl은 프로바이더의 버전관리 파일입니다.

---
+++ infra/terraform/provider.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_providers {
+    ncloud = {
+      source = "NaverCloudPlatform/ncloud"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "ncloud" {
+  access_key  = var.ncp_access_key
+  secret_key  = var.ncp_secret_key
+  region      = "KR"
+  site        = "public"
+  support_vpc = true
+}
