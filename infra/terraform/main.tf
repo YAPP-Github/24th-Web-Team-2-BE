6f0074b942b7fc45262debcd9a316dee1dadfc0c
LeoBang <bluke8489@gmail.com>
2024-07-08 22:31:20 +0900

    infra: L7 로드밸런서 정의

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -15,3 +15,15 @@ module "server" {
   vpc_id    = module.vpc.vpc_id
   subnet_id = module.vpc.subnet_id
 }
+
+module "load-balancer" {
+  source = "./modules/load-balancer"
+
+  zone         = var.zone
+  service_name = var.service_name
+  target_port  = var.facing_port
+
+  vpc_id                  = module.vpc.vpc_id
+  subnet_id               = module.vpc.subnet_id
+  temp_server_instance_no = module.server.temp_server_instance_no
+}
---
+++ infra/terraform/modules/load-balancer/variables.tf
@@ -0,0 +1,7 @@
+variable "service_name" {}
+variable "zone" {}
+variable "target_port" {}
+
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "temp_server_instance_no" {}
