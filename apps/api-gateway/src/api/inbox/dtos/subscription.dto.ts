30e2c0f993e3327dba10cbe3662022cb56a66ebb
Khan / 이창민 <EeeasyCode@gmail.com>
2024-08-05 23:32:17 +0900

    chore: inbox controller에서 사용될 DTO 정의

---
+++ apps/api-gateway/src/api/inbox/dtos/subscription.dto.ts
@@ -0,0 +1,3 @@
+export class SubscriptionDTO {
+  subscriptions: string[];
+}
--- apps/api-gateway/src/api/inbox/inbox.controller.ts
+++ apps/api-gateway/src/api/inbox/inbox.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Post, Request } from '@nestjs/common';
 import { AuthInfo } from '../../common/decorators/auth-info.decorator';
 import { InboxService } from './inbox.service';
+import { SubscriptionDTO } from './dtos/subscription.dto';
+import { SpamDTO } from './dtos/spam.dto';
+import { InterestDTO } from './dtos/Interest.dto';
 
 interface AuthInfo {
   authId: string;
@@ -15,19 +18,19 @@ export class InboxController {
   constructor(private readonly inboxService: InboxService) {}
 
   @Post('/subscriptions')
-  async addSubscription(@AuthInfo() authInfo: AuthInfo, @Body() subscriptionDTO) {
+  async addSubscription(@AuthInfo() authInfo: AuthInfo, @Body() subscriptionDTO: SubscriptionDTO) {
     const { subscriptions } = subscriptionDTO;
     return await this.inboxService.addSubscription(authInfo.userId, subscriptions);
   }
 
   @Post('/spams')
-  async addSpam(@AuthInfo() authInfo: AuthInfo, @Body() spamDTO) {
+  async addSpam(@AuthInfo() authInfo: AuthInfo, @Body() spamDTO: SpamDTO) {
     const { spams } = spamDTO;
     return await this.inboxService.addSpam(authInfo.userId, spams);
   }
 
   @Post('/interests')
-  async addInterest(@AuthInfo() authInfo: AuthInfo, @Body() interestDTO) {
+  async addInterest(@AuthInfo() authInfo: AuthInfo, @Body() interestDTO: InterestDTO) {
     const { interests } = interestDTO;
     return await this.inboxService.addInterest(authInfo.userId, interests);
   }
