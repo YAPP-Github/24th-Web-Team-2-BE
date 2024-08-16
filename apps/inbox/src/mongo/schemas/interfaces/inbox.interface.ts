7c77e95f0cfdb2b592ea47029381f24aeab91f06
LeoBang <bluke8489@gmail.com>
2024-08-16 11:43:32 +0900

    feat: 베타 서비스에 맞게 MongoDB 스키마 적용
    - subscriptions에 name 필드 추가
    - groups 필드 추가

--- apps/inbox/src/mongo/schemas/interfaces/inbox.interface.ts
+++ apps/inbox/src/mongo/schemas/interfaces/inbox.interface.ts
@@ -1,10 +1,15 @@
-import { Document } from 'mongoose';
+import { Document, ObjectId, Types } from 'mongoose';
 
+interface Group extends Document {
+  name: string;
+  senders: { name: string; address: string }[];
+}
 export interface Inbox extends Document {
   readonly userId: string;
   readonly interests: [string];
-  readonly subscriptions: [string];
-  readonly spams: [string];
+  readonly subscriptions: { name: string; address: string }[];
+  readonly groups: Types.DocumentArray<Group>;
+  readonly spams: string[];
   readonly createdAt?: Date;
   readonly updatedAt?: Date;
 }
