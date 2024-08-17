ef640b5dfdc4997ca65a8033dc80ef44582c3a0c
LeoBang <bluke8489@gmail.com>
2024-08-17 14:12:52 +0900

    feat: mailClient 수정
    - mailContextService를 적용하도록 수정
    
    - 해당 page token에 메시지 없을 경우 data에 message = undefined로 응답하는데 이 경우 빈 배열로 채워넣어 에러 예방

--- apps/mail-integrator/src/google-api/google-mail.client.ts
+++ apps/mail-integrator/src/google-api/google-mail.client.ts
@@ -3,19 +3,24 @@ import { GoogleMailFactory } from './google-mail.factory';
 import { gmail_v1 } from 'googleapis';
 
 import gmailPageTokenCache from './caches/gmail-pageToken.cache';
+import { MailContextService } from './mail-context.service';
 
 @Injectable()
 export class GoogleMailClient {
-  constructor(private readonly googleMailFactory: GoogleMailFactory) {}
+  constructor(
+    private readonly googleMailFactory: GoogleMailFactory,
+    private readonly mailContextService: MailContextService,
+  ) {}
 
   async messages(): Promise<gmail_v1.Schema$Message[]> {
     const gmail = await this.googleMailFactory.gmail();
     const messageIdentifiers = await this.messageIdentifiers();
-    const messagePromise = messageIdentifiers.messages.map((message) => {
+    const messagePromise = messageIdentifiers.map((message) => {
       const res = gmail.users.messages.get({
         userId: 'me',
         id: message.id,
         format: 'full',
+        ...this.mailContextService.getMessagesFetchOption(),
       });
       return res;
     });
@@ -45,8 +50,10 @@ export class GoogleMailClient {
     const res = await gmail.users.messages.list({
       userId: 'me',
       pageToken: gmailPageTokenCache.get('userId'),
+      ...this.mailContextService.getMessagesListOption(),
     });
     gmailPageTokenCache.set('userId', res.data.nextPageToken);
-    return res.data;
+
+    return res.data.messages ?? [];
   }
 }
