d6f1d7e3919d7a7fe8ba7b27f9793457bc0a3945
LeoBang <bluke8489@gmail.com>
2024-08-18 15:14:55 +0900

    feat: mail-integrator 서비스 외부로 노출 (controller)
    - get-mail-senders
    - get-unread-messages

--- apps/mail-integrator/src/mail-integrator.controller.ts
+++ apps/mail-integrator/src/mail-integrator.controller.ts
@@ -7,8 +7,43 @@ import { MessagePattern } from '@nestjs/microservices';
 export class MailIntegratorController {
   constructor(private readonly mailIntegratorService: MailIntegratorService) {}
 
-  @MessagePattern({ cmd: 'process-messages-onboarding' })
-  async processMessages(data: { userId: string }) {
-    await this.mailIntegratorService.processMessagesOnboarding(data.userId);
+  @MessagePattern({ cmd: 'get-mail-senders' })
+  async getMailSenders(data: { userId: string }) {
+    const result = await this.mailIntegratorService.getMailSenders(data.userId);
+    return {
+      senders: result.map((sender) => {
+        const { messageId, labels, ...rest } = sender;
+        return {
+          mailId: messageId,
+          ...rest,
+        };
+      }),
+    };
+  }
+
+  @MessagePattern({ cmd: 'get-unread-messages' })
+  async getUnreadMessages(data: { userId: string; type: 'SENDER' | 'GROUP' | 'ALL'; target?: string }) {
+    // TODO inbox API 호출
+    const inbox = {} as any; // user Id로 가져온 inbox 엔티티
+    let addresses = [];
+    switch (data.type) {
+      case 'SENDER':
+        addresses.push(data.target);
+      case 'GROUP':
+        addresses = inbox.groups.find((group) => group.name === data.target).senders.map((sender) => sender.address);
+      case 'ALL':
+        addresses = inbox.subscriptions.map((sub) => sub.address);
+    }
+
+    const messages = await this.mailIntegratorService.getUnreadMessages(data.userId, addresses);
+    return {
+      mails: messages.map((message) => {
+        const { messageId, labels, ...rest } = message;
+        return {
+          mailId: messageId,
+          ...rest,
+        };
+      }),
+    };
   }
 }
