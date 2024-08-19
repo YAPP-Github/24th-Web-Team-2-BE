4a6881e07fc4ceea22ca119f7663cb09348b47f0
Khan / 이창민 <EeeasyCode@gmail.com>
2024-08-19 18:13:19 +0900

    chore: libs 모듈에 custom filter 추가

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,5 @@ export * from './common.module';
 export * from './common.service';
 export * from './constants/constants.module';
 export * from './constants/mail.constants';
+export * from './exceptions/filter/custom-rpc.exception.filter';
+export * from './exceptions/filter/custom-axios.exception.filter';
