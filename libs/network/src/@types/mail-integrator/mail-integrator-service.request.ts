bdf3d0f7312b5b2ddce84d04ccc343c2410ed795
LeoBang <bluke8489@gmail.com>
2024-08-24 14:53:05 +0900

    feat: mail-integrator client 정의

---
+++ libs/network/src/@types/mail-integrator/mail-integrator-service.request.ts
@@ -0,0 +1,19 @@
+export type GetMailSendersRequest = {
+  userId: string;
+};
+
+export type GetUnreadMessagesRequest = {
+  userId: string;
+  type: 'SENDER' | 'GROUP' | 'ALL';
+  target?: string;
+};
+
+export type ModifyMessageAsReadRequest = {
+  userId: string;
+  mailId: string;
+};
+
+export type ModifyMessageAsUnReadRequest = {
+  userId: string;
+  mailId: string;
+};
--- libs/network/src/index.ts
+++ libs/network/src/index.ts
@@ -4,9 +4,14 @@ export * from './@types/inbox/inbox-service.response';
 export * from './@types/inbox/inbox-service.request';
 export * from './@types/auth/auth-service.request';
 export * from './@types/auth/auth-service.response';
+export * from './@types/mail-integrator/mail-integrator-service.request';
+export * from './@types/mail-integrator/mail-integrator-service.response';
 
 export * from './inbox/inbox-commad.token';
 export * from './inbox/inbox.client';
 
 export * from './auth/auth.client';
 export * from './auth/auth-command.token';
+
+export * from './mail-integrator/mail-integrator.client';
+export * from './mail-integrator/mail-integrator-command.token';
