b948aec4b72a35595996ed38ac8014468c0e284b
LeoBang <bluke8489@gmail.com>
2024-08-24 13:45:39 +0900

    feat: inbox network 클라이언트 정의

---
+++ libs/network/src/index.ts
@@ -0,0 +1,6 @@
+export * from './network.module';
+
+export * from './@types/inbox/inbox-service.response';
+export * from './@types/inbox/inbox-service.request';
+export * from './inbox/inbox-commad.token';
+export * from './inbox/inbox.client';
