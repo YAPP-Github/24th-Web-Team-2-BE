a2794c868a307a57ce4273151107f19b667ab196
LeoBang <bluke8489@gmail.com>
2024-08-30 00:21:57 +0900

    feat: network 모듈에 get-message api 추가

--- libs/network/src/mail-integrator/mail-integrator-command.token.ts
+++ libs/network/src/mail-integrator/mail-integrator-command.token.ts
@@ -1,6 +1,7 @@
 export enum MailIntegratorCommandToken {
   GET_MAIL_SENDERS = 'get-mail-senders',
   GET_UNREAD_MESSAGES = 'get-unread-messages',
+  GET_MESSAGE = 'get-message',
 
   MODIFY_MESSAGE_AS_READ = 'modify-message-as-read',
   MODIFY_MESSAGE_AS_UNREAD = 'modify-message-as-unread',
