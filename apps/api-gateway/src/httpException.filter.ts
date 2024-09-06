30ad952d122c85f9098e27ef2426fe11f7865f7b
dladncks1217 <dlaxodud1217@gmail.com>
2024-09-06 21:38:34 +0900

    feat: 에러 로깅 추가

--- apps/api-gateway/src/httpException.filter.ts
+++ apps/api-gateway/src/httpException.filter.ts
@@ -1,13 +1,17 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, Logger } from '@nestjs/common';
 import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger('HttpExceptionFilter');
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
     const err = exception.getResponse() as { message: any; statusCode: number } | { error: string; statusCode: 400; message: string[] }; // 에러내용 // class-validator
+
+    this.logger.error(`Error: ${status} - ${JSON.stringify(err.message)}`, exception.stack);
 
     if (typeof err !== 'string' && err.statusCode === 400) {
       return response.status(status).json({
