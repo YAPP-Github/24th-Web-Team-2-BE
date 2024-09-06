a12480710e65036a710a9b1dd391b19562975a84
dladncks1217 <dlaxodud1217@gmail.com>
2024-09-06 21:33:12 +0900

    feat: HttpException 인터셉터 추가

---
+++ apps/api-gateway/src/httpException.filter.ts
@@ -0,0 +1,26 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const err = exception.getResponse() as { message: any; statusCode: number } | { error: string; statusCode: 400; message: string[] }; // 에러내용 // class-validator
+
+    if (typeof err !== 'string' && err.statusCode === 400) {
+      return response.status(status).json({
+        success: false,
+        code: status,
+        data: err.message,
+      });
+    }
+
+    response.status(status).json({
+      success: false,
+      code: status,
+      data: err.message,
+    });
+  }
+}
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
+import { HttpExceptionFilter } from './httpException.filter';
 // import Redis from 'ioredis';
 // import RedisStore from 'connect-redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   // const redisClient = new Redis({
   //   host: process.env.REDIS_HOST,
   //   port: parseInt(process.env.REDIS_PORT),
